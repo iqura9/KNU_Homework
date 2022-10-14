47b5884f115cbda67470aa6a9663d60e2a0cf229
iqura9 <77846496+iqura9@users.noreply.github.com>
2022-10-14 09:33:02 +0300

    first_homework_winforms

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Show info about input(TextBox main page)
+        {
+            string input = textBox1.Text;
+            if (!long.TryParse(input, out long number))
+            {
+                MessageBox.Show("Ви ввели некоректне число", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            MessageBox.Show($"Ви ввели число {number}", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
