ac2a3c095db5eec91e40a891574af5fa8b122205
iqura9 <77846496+iqura9@users.noreply.github.com>
2022-10-14 09:15:43 +0300

    First homework

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// .NET 6.0
+
+string input = Console.ReadLine();
+if (!long.TryParse(input, out long number))
+{
+    Console.WriteLine("Ви ввели некоректне число");
+    return;
+}
+Console.WriteLine("Ви ввели число N {0}", number);
